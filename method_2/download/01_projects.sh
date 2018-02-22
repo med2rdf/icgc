1f93fe89158061c347ccfa7775eb7ed62628d269
Ubuntu <ubuntu@ip-172-31-11-21.ap-northeast-1.compute.internal>
2018-02-22 03:29:51 +0000

    restore download scripts

--- method_2/download/01_projects.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script modify projects.tsv, which can be obtained from the original ICGC portal.
-#
-# sh 00_projects.sh projects_2016_09_20_11_54_43.tsv
-
-echo "code	name	primary_site	tumour_type	tumour_subtype	countries" > projects.tsv
-
-cat $1 | \
-awk 'BEGIN {FS="\t"; OFS="\t"} NR>1 {print $1, $2, $3, "", "", $4}' >> projects.tsv
