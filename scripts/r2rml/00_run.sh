66f737de43bbe769c300f3f588be33012af70554
ryotas <ryotayamanaka@gmail.com>
2019-01-19 14:47:41 +0700

    2019-01 2

--- scripts/r2rml/00_run.sh
+++ scripts/r2rml/00_run.sh
@@ -1,5 +1,5 @@
 sqlplus icgc_user/oracle@orclpdb1 @01_staging.sql
-sqlldr  icgc_user/oracle@orclpdb1 02_stating.ctl
+sqlldr  icgc_user/oracle@orclpdb1 02_staging.ctl
 sqlplus icgc_user/oracle@orclpdb1 @03_rdfview.sql
 #sqlplus icgc_user/oracle @04_export.sql
 #sqlplus icgc_user/oracle @05_spool.sql > /dev/null
